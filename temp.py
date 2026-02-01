5010e90970589c03cd4f5b2a7f289cd5bf397305
raynbowy23 <lov.us.basketball@gmail.com>
2026-02-01 16:18:53 -0600

    refactor: set python version more than 3.10

--- temp.py
+++ temp.py
@@ -1,5 +1,12 @@
+import pymupdf.layout
 import pymupdf4llm
 
-md_text = pymupdf4llm.to_markdown("../resource/chap14.pdf")
-with open("chap14.md", "w", encoding="utf-8") as f:
+# for i in range(1, 38):
+#     doc = pymupdf.open(f"../resource/chap{i}.pdf")
+#     md_text = pymupdf4llm.to_markdown(doc)
+#     with open(f"chap{i}.md", "w", encoding="utf-8") as f:
+#         f.write(md_text)
+doc = pymupdf.open(f"../resource/Highway_Capacity_Manual_Edition_7.1_Chapters.pdf")
+md_text = pymupdf4llm.to_markdown(doc)
+with open("additional.md", "w", encoding="utf-8") as f:
     f.write(md_text)
